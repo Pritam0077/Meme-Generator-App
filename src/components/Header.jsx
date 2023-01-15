1728593b7b2eb5c53c46d1f9f878218212e21079
shyamtawli <shyamtawli2@gmail.com>
2023-01-15 12:02:19 +0530

    mobile responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ export default function Header(props) {
   return (
     <div>
       <header className="header">
-      <nav class="navbar">
+      <nav className="navbar">
           <img className="header--image" alt="trollfaceimage" src={headerimage} /> 
           <h2 className="header--title">Meme-Generator</h2> 
           <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked"  onClick={props.toogleMode}/>
-                <label class="form-check-label" for="flexSwitchCheckChecked">Dark mode</label>
+                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked"  onClick={props.toogleMode}/>
+                <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Dark mode</label>
           </div>
         </nav>
       </header>
